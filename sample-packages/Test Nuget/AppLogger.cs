12c8b07906ec8a777c08dcfc50c92e7c07d712fc
Drescher86 <mortenpdrescher@gmail.com>
2023-11-16 07:35:32 +0100

    Added nuget project

---
+++ sample-packages/Test Nuget/AppLogger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Nuget
+{
+    public class AppLogger
+    {
+        public void Log(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
